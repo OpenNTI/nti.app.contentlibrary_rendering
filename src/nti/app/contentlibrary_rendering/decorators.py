270750d0561142b69cd3c57a69ebcb32d3ab4a46
Josh Zuech <josh.zuech@nextthought.com>
2017-02-04 17:21:44 +0000

    Expose most recent render job on renderable content packages
    git-svn-id: https://repos.nextthought.com/svn/nti-svn/nti.app.contentlibrary_rendering/trunk@105825 3dfb626b-079f-4ab1-a9f6-4654e5377508

---
+++ src/nti/app/contentlibrary_rendering/decorators.py
@@ -0,0 +1,44 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+"""
+.. $Id$
+"""
+
+from __future__ import print_function, unicode_literals, absolute_import, division
+__docformat__ = "restructuredtext en"
+
+logger = __import__('logging').getLogger(__name__)
+
+from zope import component
+from zope import interface
+
+from zope.location.interfaces import ILocation
+
+from pyramid.interfaces import IRequest
+
+from nti.app.renderers.decorators import AbstractAuthenticatedRequestAwareDecorator
+
+from nti.contentlibrary.interfaces import IRenderableContentPackage
+
+from nti.contentlibrary_rendering.interfaces import IContentPackageRenderMetadata
+
+from nti.externalization.interfaces import IExternalMappingDecorator
+
+@interface.implementer(IExternalMappingDecorator)
+@component.adapter(IRenderableContentPackage, IRequest)
+class _RenderablePackageEditorDecorator(AbstractAuthenticatedRequestAwareDecorator):
+    """
+    Decorates IRenderableContentPackage with render info.
+    """
+
+    def _predicate(self, context, result):
+        return      self._is_authenticated \
+                and has_permission(ACT_CONTENT_EDIT, context, self.request)
+
+    def _do_decorate_external(self, context, result):
+        meta = IContentPackageRenderMetadata( context, None )
+        if meta is not None:
+            latest_job = meta.mostRecentRenderJob()
+            if latest_job is not None:
+                result['LatestRenderJob'] = latest_job
+
