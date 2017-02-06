f5a830ee026b97fe8b57d6c71886ba24947f2355
Carlos Sanchez <carlos.sanchez@nextthought.com>
2017-02-06 23:42:58 +0000

    App layer S3 locator
    git-svn-id: https://repos.nextthought.com/svn/nti-svn/nti.app.contentlibrary_rendering/trunk@105946 3dfb626b-079f-4ab1-a9f6-4654e5377508

---
+++ src/nti/app/contentlibrary_rendering/locators.py
@@ -0,0 +1,26 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+"""
+.. $Id$
+"""
+
+from __future__ import print_function, unicode_literals, absolute_import, division
+__docformat__ = "restructuredtext en"
+
+logger = __import__('logging').getLogger(__name__)
+
+
+from zope import component
+
+from nti.appserver.interfaces import IApplicationSettings
+
+from nti.contentlibrary_rendering.locators import S3Locator as BaseS3Locator
+
+from nti.property.property import Lazy
+
+
+class S3Locator(BaseS3Locator):
+
+    @Lazy
+    def settings(self):
+        return component.queryUtility(IApplicationSettings) or {}
