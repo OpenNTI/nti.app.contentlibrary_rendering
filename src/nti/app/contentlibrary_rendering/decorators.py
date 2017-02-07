ea40baf497ebe703622113ee56ce10021645626e
Josh Zuech <josh.zuech@nextthought.com>
2017-02-07 19:57:50 +0000

    Add a view to fetch render job status
    git-svn-id: https://repos.nextthought.com/svn/nti-svn/nti.app.contentlibrary_rendering/trunk@106024 3dfb626b-079f-4ab1-a9f6-4654e5377508

--- src/nti/app/contentlibrary_rendering/decorators.py
+++ src/nti/app/contentlibrary_rendering/decorators.py
@@ -9,10 +9,14 @@ __docformat__ = "restructuredtext en"
 
 logger = __import__('logging').getLogger(__name__)
 
+from pyramid.interfaces import IRequest
+
 from zope import component
 from zope import interface
 
-from pyramid.interfaces import IRequest
+from zope.location.interfaces import ILocation
+
+from nti.app.contentlibrary_rendering import VIEW_QUERY_JOB
 
 from nti.app.renderers.decorators import AbstractAuthenticatedRequestAwareDecorator
 
@@ -24,7 +28,17 @@ from nti.contentlibrary_rendering.interfaces import IContentPackageRenderMetadat
 
 from nti.dataserver.authorization import ACT_CONTENT_EDIT
 
+from nti.externalization.interfaces import StandardExternalFields
 from nti.externalization.interfaces import IExternalMappingDecorator
+
+from nti.links.links import Link
+
+LINKS = StandardExternalFields.LINKS
+
+def _package_url_path(package, request):
+    ds2 = request.path_info_peek()
+    path = '%s/Library/%s' % (ds2, package.ntiid)
+    return path
 
 
 @interface.implementer(IExternalMappingDecorator)
@@ -43,4 +57,15 @@ class _RenderablePackageEditorDecorator(AbstractAuthenticatedRequestAwareDecorat
         if meta is not None:
             latest_job = meta.mostRecentRenderJob()
             if latest_job is not None:
+                # Decorate the job itself along with a link to fetch job status.
                 result['LatestRenderJob'] = latest_job
+                _links = result.setdefault(LINKS, [])
+                path = _package_url_path( self.context, self.request )
+                link = Link(path,
+                            rel=VIEW_QUERY_JOB,
+                            elements=(VIEW_QUERY_JOB,),
+                            params={'job_id': latest_job.job_id})
+                interface.alsoProvides(link, ILocation)
+                link.__name__ = ''
+                link.__parent__ = context
+                _links.append(link)
---
+++ src/nti/app/contentlibrary_rendering/views/general_views.py
@@ -0,0 +1,68 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+"""
+.. $Id$
+"""
+
+from __future__ import print_function, unicode_literals, absolute_import, division
+__docformat__ = "restructuredtext en"
+
+logger = __import__('logging').getLogger(__name__)
+
+from pyramid import httpexceptions as hexc
+
+from pyramid.view import view_config
+from pyramid.view import view_defaults
+
+from requests.structures import CaseInsensitiveDict
+
+from nti.app.base.abstract_views import AbstractAuthenticatedView
+
+from nti.app.contentlibrary_rendering import VIEW_QUERY_JOB
+
+from nti.app.contentlibrary_rendering.views import MessageFactory as _
+
+from nti.contentlibrary.interfaces import IRenderableContentPackage
+
+from nti.contentlibrary_rendering.interfaces import IContentPackageRenderMetadata
+
+from nti.dataserver import authorization as nauth
+
+
+@view_config(context=IRenderableContentPackage)
+@view_defaults(route_name='objects.generic.traversal',
+               renderer='rest',
+               request_method='GET',
+               name=VIEW_QUERY_JOB,
+               permission=nauth.ACT_CONTENT_EDIT)
+class QueryJobView(AbstractAuthenticatedView):
+    """
+    Fetch the render job for the contextual `IContentPackage`. Optionally,
+    a specific job status can be asked for via `job_id`.
+    """
+
+    def __call__(self):
+        params = CaseInsensitiveDict(self.request.params)
+        job_id = params.get( 'JobId' ) or params.get( 'job' ) or params.get( 'job_id' )
+        meta = IContentPackageRenderMetadata( self.context, None )
+        if meta is None:
+            logger.warn( 'No meta found for content package (%s)', self.context.ntiid )
+            raise hexc.HTTPNotFound(_('Content has not been processed.'))
+
+        if job_id:
+            try:
+                render_job = meta[job_id]
+            except KeyError:
+                logger.warn( 'No job found for content package (%s) (%s)',
+                             self.context.ntiid, job_id )
+                raise hexc.HTTPNotFound(_('No content found for job key.'))
+        else:
+            render_job = meta.mostRecentRenderJob()
+
+        if render_job is None:
+            logger.warn( 'No job found for content package (%s)',
+                         self.context.ntiid )
+            raise hexc.HTTPNotFound(_('Content has not been processed.'))
+        return render_job
+
+
