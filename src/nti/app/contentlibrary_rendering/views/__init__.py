0897a8c8b88e9d65821088d6ed0bc97ef093def9
Carlos Sanchez <carlos.sanchez@nextthought.com>
2017-02-08 19:00:48 +0000

    use common message factory
    git-svn-id: https://repos.nextthought.com/svn/nti-svn/nti.app.contentlibrary_rendering/trunk@106107 3dfb626b-079f-4ab1-a9f6-4654e5377508

--- src/nti/app/contentlibrary_rendering/views/__init__.py
+++ src/nti/app/contentlibrary_rendering/views/__init__.py
@@ -9,5 +9,4 @@ __docformat__ = "restructuredtext en"
 
 logger = __import__('logging').getLogger(__name__)
 
-import zope.i18nmessageid
-MessageFactory = zope.i18nmessageid.MessageFactory(__name__)
+from nti.app.contentlibrary import MessageFactory
