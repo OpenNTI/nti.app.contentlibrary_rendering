cb21e983d7dea2e91a80424387882b9a55039d9c
Carlos Sanchez <carlos.sanchez@nextthought.com>
2017-03-09 02:09:41 +0000

    make sure remove job is queued
    git-svn-id: https://repos.nextthought.com/svn/nti-svn/nti.app.contentlibrary_rendering/trunk@108355 3dfb626b-079f-4ab1-a9f6-4654e5377508

--- src/nti/app/contentlibrary_rendering/subscribers.py
+++ src/nti/app/contentlibrary_rendering/subscribers.py
@@ -18,6 +18,7 @@ from zope.intid.interfaces import IIntIdRemovedEvent
 from nti.app.authentication import get_remote_user
 
 from nti.contentlibrary.interfaces import IRenderableContentPackage
+from nti.contentlibrary.interfaces import IContentPackageLocationChanged
 
 from nti.contentlibrary_rendering.utils import render_package
 from nti.contentlibrary_rendering.utils import remove_renderered_package
@@ -40,3 +41,10 @@ def _content_published(package, event):
 @component.adapter(IRenderableContentPackage, IIntIdRemovedEvent)
 def _content_removed(package, event):
     remove_renderered_package(package)
+
+
+@component.adapter(IRenderableContentPackage, IContentPackageLocationChanged)
+def _content_location_changed(package, event):
+    old_root = event.old_root
+    if old_root is not None:
+        remove_renderered_package(package, old_root)
