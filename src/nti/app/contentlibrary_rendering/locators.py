8acaf0aabdaac37d3c1c860299eef49553a81857
Carlos Sanchez <carlos.sanchez@nextthought.com>
2017-03-09 17:13:05 +0000

    no need for locking
    git-svn-id: https://repos.nextthought.com/svn/nti-svn/nti.app.contentlibrary_rendering/trunk@108418 3dfb626b-079f-4ab1-a9f6-4654e5377508

--- src/nti/app/contentlibrary_rendering/locators.py
+++ src/nti/app/contentlibrary_rendering/locators.py
@@ -9,63 +9,21 @@ __docformat__ = "restructuredtext en"
 
 logger = __import__('logging').getLogger(__name__)
 
-import functools
-
 from zope import component
-
-from nti.app.contentlibrary_rendering import LOCK_TIMEOUT
-from nti.app.contentlibrary_rendering import SYNC_LOCK_NAME
 
 from nti.appserver.interfaces import IApplicationSettings
 
 from nti.contentlibrary_rendering.locators import S3Locator as BaseS3Locator
 from nti.contentlibrary_rendering.locators import FilesystemLocator as BaseFilesystemLocator
 
-from nti.dataserver.interfaces import IRedisClient
-
 from nti.property.property import Lazy
-
-
-def redis():
-    return component.getUtility(IRedisClient)
-
-
-def acquire(blocking=True):
-    lock = redis().lock(SYNC_LOCK_NAME, LOCK_TIMEOUT)
-    try:
-        acquired = lock.acquire(blocking=blocking)
-        if acquired:
-            return lock
-    except Exception:
-        logger.exception("Cannot get a redis lock")
-    return None
-
-
-def release(lock=None):
-    try:
-        if lock is not None:
-            lock.release()
-    except Exception:
-        logger.exception("Error while releasing Sync lock")
-
-def needs_lock(func):
-    @functools.wraps(func)
-    def wrapper(self, *args, **kw):
-        lock = acquire()
-        try:
-            return func(self, *args, **kw)
-        finally:
-            release(lock)
-    return wrapper
 
 
 class FilesystemLocator(BaseFilesystemLocator):
 
-    @needs_lock
     def _do_locate(self, path, root, context):
         return BaseFilesystemLocator._do_locate(self, path, root, context)
 
-    @needs_lock
     def _do_remove(self, bucket):
         BaseFilesystemLocator._do_remove(self, bucket)
 
@@ -76,10 +34,8 @@ class S3Locator(BaseS3Locator):
     def settings(self):
         return component.queryUtility(IApplicationSettings) or {}
 
-    @needs_lock
     def _do_locate(self, path, root, context, debug=True):
         return BaseS3Locator._do_locate(self, path, root, context, debug=debug)
 
-    @needs_lock
     def _do_remove(self, bucket, debug=True):
         return BaseS3Locator._do_remove(self, bucket, debug=debug)
