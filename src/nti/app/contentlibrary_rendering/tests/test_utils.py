1cebbfd9e315935386d96d74a569ba45ca4c0cac
Carlos Sanchez <carlos.sanchez@nextthought.com>
2017-03-21 15:11:19 +0000

    add test
    git-svn-id: https://repos.nextthought.com/svn/nti-svn/nti.app.contentlibrary_rendering/trunk@109248 3dfb626b-079f-4ab1-a9f6-4654e5377508

---
+++ src/nti/app/contentlibrary_rendering/tests/test_utils.py
@@ -0,0 +1,64 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+from __future__ import print_function, unicode_literals, absolute_import, division
+__docformat__ = "restructuredtext en"
+
+# disable: accessing protected members, too many methods
+# pylint: disable=W0212,R0904
+
+from hamcrest import is_not
+from hamcrest import has_length
+from hamcrest import assert_that
+does_not = is_not
+
+import fudge
+
+from zope import component
+
+from zope.intid.interfaces import IIntIds
+
+from nti.app.contentlibrary_rendering.utils import get_pending_render_jobs
+
+from nti.contentlibrary_rendering.index import get_contentrenderjob_catalog
+
+from nti.contentlibrary_rendering.model import ContentPackageRenderJob
+
+from nti.app.contentlibrary_rendering.tests import ContentlibraryRenderingLayerTest
+
+from nti.dataserver.tests import mock_dataserver
+
+
+class TestUtils(ContentlibraryRenderingLayerTest):
+
+    @mock_dataserver.WithMockDS
+    @fudge.patch("nti.contentlibrary_rendering.index.find_interface")
+    def test_get_pending_render_jobs(self, mock_fi):
+        job = ContentPackageRenderJob()
+        job.state = 'Pending'
+        job.jobId = 'tag:nextthought.com,2011-10:NTI-RenderJob-58'
+        job.package = 'tag:nextthought.com,2011-10:NTI-HTML-58'
+
+        fake_folder = fudge.Fake()
+        fake_folder.__name__ = 'janux.dev'
+        mock_fi.is_callable().with_args().returns(fake_folder)
+
+        with mock_dataserver.mock_db_trans(self.ds):
+            intids = component.getUtility(IIntIds)
+            current_transaction = mock_dataserver.current_transaction
+            current_transaction.add(job)
+            intids.register(job)
+            doc_id = intids.getId(job)
+
+            catalog = get_contentrenderjob_catalog()
+            catalog.index_doc(doc_id, job)
+
+            jobs = get_pending_render_jobs(sites='janux.dev')
+            assert_that(jobs, has_length(1))
+
+            jobs = get_pending_render_jobs(sites='janux.dev',
+                                           packages='tag:nextthought.com,2011-10:NTI-HTML-58')
+            assert_that(jobs, has_length(1))
+
+            jobs = get_pending_render_jobs(sites='janux.dev', packages='foo')
+            assert_that(jobs, has_length(0))
