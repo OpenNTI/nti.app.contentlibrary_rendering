d1f8327d1a20f5423b5a3d148d71d27227714786
Carlos Sanchez <carlos.sanchez@nextthought.com>
2017-03-21 14:35:21 +0000

    make sure we query for pending state
    git-svn-id: https://repos.nextthought.com/svn/nti-svn/nti.app.contentlibrary_rendering/trunk@109245 3dfb626b-079f-4ab1-a9f6-4654e5377508

--- src/nti/app/contentlibrary_rendering/utils.py
+++ src/nti/app/contentlibrary_rendering/utils.py
@@ -16,22 +16,25 @@ from zope import component
 from zope.intid.interfaces import IIntIds
 
 from nti.contentlibrary_rendering.index import IX_SITE
+from nti.contentlibrary_rendering.index import IX_STATE
 from nti.contentlibrary_rendering.index import IX_PACKAGE_NTIID
 from nti.contentlibrary_rendering.index import get_contentrenderjob_catalog
 
+from nti.contentlibrary_rendering.interfaces import PENDING
 from nti.contentlibrary_rendering.interfaces import IContentPackageRenderJob
 
 from nti.site.site import get_component_hierarchy_names
+
 
 def get_pending_render_jobs(sites=(), packages=()):
     if not sites:
         sites = get_component_hierarchy_names()
     elif isinstance(sites, six.string_types):
         sites = sites.split()
-        
+
     if isinstance(packages, six.string_types):
         packages = packages.split()
- 
+
     intids = component.getUtility(IIntIds)
     catalog = get_contentrenderjob_catalog()
     if catalog is None:  # tests
@@ -39,14 +42,15 @@ def get_pending_render_jobs(sites=(), packages=()):
 
     query = {
         IX_SITE: {'any_of': sites},
+        IX_STATE: {'any_of': (PENDING,)}
     }
     if packages:
         query[IX_PACKAGE_NTIID] = {'any_of': packages}
-    
+
     result = list()
     for doc_id in catalog.apply(query) or ():
         context = intids.queryObject(doc_id)
         if      IContentPackageRenderJob.providedBy(context) \
-            and context.is_pending():
+                and context.is_pending():
             result.append(context)
     return result
