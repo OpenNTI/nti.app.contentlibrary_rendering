a8b03997e0f8a463bc49d7e477d7f05c6a8a6438
Carlos Sanchez <carlos.sanchez@nextthought.com>
2017-03-26 01:34:36 +0000

    implement view
    git-svn-id: https://repos.nextthought.com/svn/nti-svn/nti.app.contentlibrary_rendering/trunk@109637 3dfb626b-079f-4ab1-a9f6-4654e5377508

--- src/nti/app/contentlibrary_rendering/views/admin_views.py
+++ src/nti/app/contentlibrary_rendering/views/admin_views.py
@@ -18,6 +18,7 @@ from pyramid import httpexceptions as hexc
 from pyramid.view import view_config
 from pyramid.view import view_defaults
 
+from nti.app.base.abstract_views import get_all_sources
 from nti.app.base.abstract_views import AbstractAuthenticatedView
 
 from nti.app.externalization.view_mixins import BatchingUtilsMixin
@@ -25,10 +26,16 @@ from nti.app.externalization.view_mixins import ModeledContentUploadRequestUtils
 
 from nti.app.contentlibrary.views import LibraryPathAdapter
 
+from nti.app.contentlibrary_rendering.views import MessageFactory as _
+
 from nti.app.contentlibrary_rendering.utils import get_failed_render_jobs
 from nti.app.contentlibrary_rendering.utils import get_pending_render_jobs
 
 from nti.app.contentlibrary_rendering.views import perform_content_validation
+
+from nti.app.externalization.error import raise_json_error
+
+from nti.cabinet import NamedSource
 
 from nti.contentlibrary import RENDERABLE_CONTENT_MIME_TYPES
 
@@ -38,6 +45,8 @@ from nti.contentlibrary.interfaces import IRenderableContentPackage
 from nti.contentlibrary.utils import get_content_packages
 
 from nti.contentlibrary_rendering import QUEUE_NAMES
+
+from nti.contentlibrary_rendering.archive import render_archive
 
 from nti.contentlibrary_rendering.interfaces import IContentPackageRenderMetadata
 
@@ -249,8 +258,43 @@ class RenderContentBundleView(AbstractAuthenticatedView,
 
     MAX_SOURCE_SIZE = 524288000 # 500mb
 
+    def readInput(self, value=None):
+        result = super(RenderContentBundleView, self).readInput(value)
+        return CaseInsensitiveDict(result)
+
     def __call__(self):
-        pass
+        result = LocatedExternalDict()
+        result.__name__ = self.request.view_name
+        result.__parent__ = self.request.context
+        result[ITEMS] = items = []
+        # read params
+        data = self.readInput()
+        creator = self.remoteUser.username
+        provider = data.get('provider') or 'NTI'
+        site = data.get('site') or data.get('site_name')
+        # process sources
+        sources = get_all_sources(self.request, None)
+        for name, source in sources.items():
+            if source.length >= self.MAX_SOURCE_SIZE:
+                raise_json_error(
+                    self.request,
+                    hexc.HTTPUnsupportedMediaType,
+                    {
+                        u'message': _("Max file size exceeded"),
+                        u'code': 'MaxFileSizeExceeded',
+                    },
+                    None)
+            filename = getattr(source, 'filename', None) or name
+            # save source
+            target = NamedSource(filename, source.data)
+            # schedule
+            status_id = render_archive(target, 
+                                       creator, 
+                                       site=site,
+                                       provider=provider)
+            items.append(status_id)
+        result[ITEM_COUNT] = result[TOTAL] = len(items)
+        return result
     
 
 # queue views
