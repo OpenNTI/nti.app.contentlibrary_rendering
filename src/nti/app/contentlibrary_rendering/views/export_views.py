203144a78a24d625c31974cc954b883f976b3c77
Carlos Sanchez <carlos.sanchez@nextthought.com>
2017-03-31 18:38:32 +0000

    add export views
    git-svn-id: https://repos.nextthought.com/svn/nti-svn/nti.app.contentlibrary_rendering/trunk@109977 3dfb626b-079f-4ab1-a9f6-4654e5377508

---
+++ src/nti/app/contentlibrary_rendering/views/export_views.py
@@ -0,0 +1,94 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+"""
+.. $Id$
+"""
+
+from __future__ import print_function, unicode_literals, absolute_import, division
+__docformat__ = "restructuredtext en"
+
+logger = __import__('logging').getLogger(__name__)
+
+import os
+import shutil
+import zipfile
+import tempfile
+
+from pyramid import httpexceptions as hexc
+
+from pyramid.view import view_config
+from pyramid.view import view_defaults
+
+from nti.app.base.abstract_views import AbstractAuthenticatedView
+
+from nti.app.contentlibrary_rendering.views import MessageFactory as _
+
+from nti.app.externalization.error import raise_json_error
+
+from nti.coremetadata.interfaces import IPublishable
+
+from nti.contentlibrary.interfaces import IContentPackage
+from nti.contentlibrary.interfaces import IContentRendered 
+from nti.contentlibrary.interfaces import IFilesystemBucket 
+
+from nti.dataserver import authorization as nauth
+
+
+@view_config(context=IContentPackage)
+@view_defaults(route_name='objects.generic.traversal',
+               renderer='rest',
+               request_method='POST',
+               name="Export",
+               context=IContentPackage,
+               permission=nauth.ACT_SYNC_LIBRARY)
+class ExportContentPackageContentsView(AbstractAuthenticatedView):
+
+    def _export_fs(self, root):
+        tempdir = tempfile.mkdtemp()
+        zip_file = os.path.join(tempdir, "export.zip")
+        shutil.make_archive(zip_file, 'zip', root.absolute_path)
+        return zip_file
+
+    def _export_boto(self, pkg_key):
+        tempdir = tempfile.mkdtemp()
+        zip_file = os.path.join(tempdir, "export.zip")
+        zf = zipfile.ZipFile(zip_file,  "w")
+        try:
+            bucket = pkg_key.bucket
+            for key in bucket.list(prefix=pkg_key.name):
+                arcname = key.name[len(pkg_key.name):]
+                zf.writestr(arcname, key.get_contents_as_string())
+        finally:
+            zf.close()
+        return zip_file
+
+    def _export_response(self, zip_file, response):
+        try:
+            filename = os.path.split(zip_file)[1]
+            response.content_encoding = b'identity'
+            response.content_type = b'application/zip; charset=UTF-8'
+            content_disposition = b'attachment; filename="%s"' % filename
+            response.content_disposition = str(content_disposition)
+            response.body_file = open(zip_file, "rb")
+            return response
+        finally:
+            os.remove(zip_file)
+
+    def __call__(self):
+        if      IPublishable.providedBy(self.context) \
+            and not IContentRendered.providedBy(self.context):
+            raise_json_error(
+                    self.request,
+                    hexc.HTTPUnprocessableEntity,
+                    {
+                        u'message': _("Max file size exceeded"),
+                        u'code': 'MaxFileSizeExceeded',
+                    },
+                    None)
+        root = getattr(self.context, 'root', None)
+        if IFilesystemBucket.providedBy(root):
+            zip_file = self._export_fs(root)
+        else: # boto
+            key = self.context.key
+            self._export_boto(key)
+        return self._export_response(zip_file, self.request.response)
