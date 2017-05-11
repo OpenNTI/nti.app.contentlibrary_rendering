c9c08a9e3b133c03ab4df5d1fc4473185e966bfa
Carlos Sanchez <carlos.sanchez@nextthought.com>
2017-05-11 18:42:51 +0000

    ensure unicode; remove unicode_literals;
    git-svn-id: https://repos.nextthought.com/svn/nti-svn/nti.app.contentlibrary_rendering/trunk@112504 3dfb626b-079f-4ab1-a9f6-4654e5377508

--- src/nti/app/contentlibrary_rendering/views/sync_views.py
+++ src/nti/app/contentlibrary_rendering/views/sync_views.py
@@ -4,7 +4,7 @@
 .. $Id$
 """
 
-from __future__ import print_function, unicode_literals, absolute_import, division
+from __future__ import print_function, absolute_import, division
 __docformat__ = "restructuredtext en"
 
 logger = __import__('logging').getLogger(__name__)
@@ -85,8 +85,8 @@ class RenderContentSourceView(AbstractAuthenticatedView,
                     self.request,
                     hexc.HTTPUnprocessableEntity,
                     {
-                        u'message': _("Max file size exceeded"),
-                        u'code': 'MaxFileSizeExceeded',
+                        'message': _(u"Max file size exceeded"),
+                        'code': u'MaxFileSizeExceeded',
                     },
                     None)
             filename = getattr(source, 'filename', None) or name
@@ -118,8 +118,9 @@ class LibraryJobStatusView(AbstractAuthenticatedView):
                 self.request,
                 hexc.HTTPUnprocessableEntity,
                 {
-                    u'message': _("Must provide a job identifier"),
-                    u'code': 'InvalidJobID',
+                    'message': _(u"Must provide a job identifier"),
+                    'field': u'jobId',
+                    'code': u'InvalidJobID',
                 },
                 None)
         status = get_job_status(job_id)
@@ -150,8 +151,9 @@ class LibraryJobErrorView(AbstractAuthenticatedView):
                 self.request,
                 hexc.HTTPUnprocessableEntity,
                 {
-                    u'message': _("Must provide a job identifier"),
-                    u'code': 'InvalidJobID',
+                    'message': _(u"Must provide a job identifier"),
+                    'field': u'jobId',
+                    'code': u'InvalidJobID',
                 },
                 None)
         error = get_job_error(job_id)
