feb67d11dc2c57235408c1cfc49be42160174c77
Carlos Sanchez <carlos.sanchez@nextthought.com>
2017-05-12 15:44:53 +0000

    use common funcs
    git-svn-id: https://repos.nextthought.com/svn/nti-svn/nti.app.contentlibrary_rendering/trunk@112601 3dfb626b-079f-4ab1-a9f6-4654e5377508

--- src/nti/app/contentlibrary_rendering/views/import_views.py
+++ src/nti/app/contentlibrary_rendering/views/import_views.py
@@ -10,10 +10,7 @@ __docformat__ = "restructuredtext en"
 logger = __import__('logging').getLogger(__name__)
 
 import os
-import six
-import time
 import shutil
-import socket
 import tempfile
 
 from zope import component
@@ -40,8 +37,6 @@ from nti.cabinet.filer import transfer_to_native_file
 
 from nti.common.string import is_true
 
-from nti.contentlibrary import RENDERED_PREFIX
-
 from nti.contentlibrary.interfaces import IContentPackageLibrary
 
 from nti.contentlibrary.utils import get_content_package_site
@@ -50,16 +45,13 @@ from nti.contentlibrary_rendering._archive import move_content
 from nti.contentlibrary_rendering._archive import process_source
 from nti.contentlibrary_rendering._archive import remove_content
 from nti.contentlibrary_rendering._archive import update_library
+from nti.contentlibrary_rendering._archive import obfuscate_filename
 from nti.contentlibrary_rendering._archive import get_rendered_package_ntiid
-
-from nti.contentlibrary_rendering.common import sha1_hex_digest
 
 from nti.dataserver import authorization as nauth
 
 from nti.externalization.interfaces import LocatedExternalDict
 from nti.externalization.interfaces import StandardExternalFields
-
-from nti.namedfile.file import safe_filename
 
 from nti.site.hostpolicy import get_host_site
 
@@ -86,38 +78,25 @@ class ImportRenderedContentView(_AbstractSyncAllLibrariesView):
             site_name = getSite().__name__
         return site_name
 
-    def _hex(self, name, now=None):
-        now = now or time.time()
-        digest = sha1_hex_digest(six.binary_type(name),
-                                 six.binary_type(now),
-                                 six.binary_type(socket.gethostname()))
-        return digest[20:].upper()  # 40 char string
-
-    def _out_name(self, name):
-        hostname = socket.gethostname()
-        name = "%s_%s_%s.%s" % (RENDERED_PREFIX, hostname,
-                                name[:15], self._hex(name))
-        name = safe_filename(name)
-        return name
-
-    def _process_source(self, content, name, path, keep_name=False):
+    def _process_source(self, content, name, path, obfuscate=True):
         # seek to 0 in case of a retry
         if hasattr(content, 'seek'):
             content.seek(0)
         # transfer to local directory
         transfer_to_native_file(content, path)
         source = process_source(path)
-        if not keep_name:
-            path = os.path.split(source)[0]
-            new = os.path.join(path, self._out_name(name))
-            os.rename(source, new)
-            source = new
+        if obfuscate:
+            source = obfuscate_filename(source)
         return source
 
     def _do_call(self):
         data = self.readInput()
         request_site = data.get('site')
-        keep_name = is_true(data.get('keepName') or data.get('keep_name'))
+        obfuscate = data.get('obfuscate')
+        if obfuscate is None: # default to True
+            obfuscate = True 
+        else:
+            obfuscate = is_true(obfuscate)
         request_library = component.getUtility(IContentPackageLibrary)
         # preapre results
         result = LocatedExternalDict()
@@ -132,7 +111,7 @@ class ImportRenderedContentView(_AbstractSyncAllLibrariesView):
                 # 1. process source
                 name = getattr(source, 'name', None) or name
                 path = os.path.join(tmp_dir, name)
-                source = self._process_source(source, name, path, keep_name)
+                source = self._process_source(source, name, path, obfuscate)
                 # 2. get package ntiid
                 ntiid = get_rendered_package_ntiid(source)
                 # 3. get existing package if any
