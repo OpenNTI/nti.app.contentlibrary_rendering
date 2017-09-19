0a3e18c75f199a66fd295f099c8f3fa515529fff
Carlos Sanchez <carlos.sanchez@nextthought.com>
2017-09-19 22:38:59 +0000

    fix test
    git-svn-id: https://repos.nextthought.com/svn/nti-svn/nti.app.contentlibrary_rendering/trunk@121954 3dfb626b-079f-4ab1-a9f6-4654e5377508

--- src/nti/app/contentlibrary_rendering/docutils/tests/test_translators.py
+++ src/nti/app/contentlibrary_rendering/docutils/tests/test_translators.py
@@ -118,11 +118,10 @@ class TestTranslators(ApplicationLayerTest):
         media.ntiid = u'tag:nextthought.com,2011-10:BLEACH-NTIVideo-Ichigo.vs.Aizen'
         mock_fon.is_callable().with_args().returns(media)
         index, _ = self._generate_from_file('ntivideoref.rst')
-        assert_that(index, contains_string('<object class="ntivideoref"'))
+        assert_that(index, contains_string('<object class="ntivideo"'))
+        assert_that(index, contains_string('data-ntiid="tag:nextthought.com,2011-10:BLEACH-NTIVideo-Ichigo.vs.Aizen"'))
         assert_that(index, contains_string('<param name="visibility" value="everyone"'))
         assert_that(index, contains_string('<param name="label" value="Ichigo vs Aizen"'))
-        assert_that(index, 
-                    contains_string('<param name="mimeType" value="application/vnd.nextthought.ntivideo"'))
         assert_that(index, 
                     contains_string('<param name="ntiid" value="tag:nextthought.com,2011-10:BLEACH-NTIVideo-Ichigo.vs.Aizen"'))
 
